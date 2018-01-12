edb3e288ff021a73bc45e06c4a7fc3215aaa4f8d
Narcissus7 <18813104077@163.com>
2018-01-12 18:37:39 +0800

    1.12

--- NestedIterator.py
+++ NestedIterator.py
@@ -1,7 +1,12 @@
-from compiler.ast import flatten
+# from compiler.ast import flatten
+#
+# d=[1,2,3,[4,5],[6,7,[8,9]]]
+#
+# print flatten(d)
 
-d=[1,2,3,[4,5],[6,7,[8,9]]]
+a = 'dasda'
+a = a + 'a'
 
-print flatten(d)
 
+print (str(a))
 
---
+++ expressionExpand.py
@@ -0,0 +1,12 @@
+
+def expressionExpand(s):
+    stack = []
+    slist = list(s)
+    print(slist)
+    return 0
+
+
+
+s = 'abc3[a]'
+ss = '3[2[ad]3[pf]]xyz'
+expressionExpand(s)
